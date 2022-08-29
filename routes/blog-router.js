e1ba9cda721520009a7d2fd9b7b2aafae53e8c52
Angus <sickmi14798@gmail.com>
2022-08-29 09:25:01 +0800

    db-connect

--- routes/blog-router.js
+++ routes/blog-router.js
@@ -5,20 +5,19 @@ const pool = require('../configs/mysql')
 //RESful API
 
 // http://localhost:8080/api/blog --> 顯示所有部落格
-router.get('/', async (req, res, next) => {
+router.get('/', async (req, res) => {
   // let [data] = await pool.execute('SELECT * FROM blog ORDER BY create_time DESC')
-    let [data] = await pool.execute('SELECT blog.*, user.*, blog.id AS blog_id FROM blog JOIN user ON blog.user_id = user.id ORDER BY blog.create_time DESC')
+  let [data] = await pool.execute('SELECT blog.*, user.*, blog.id AS blog_id FROM blog JOIN user ON blog.user_id = user.id ORDER BY blog.create_time DESC')
 
-      // let [data] = await pool.execute('SELECT blog.*, category.category_name, user.*, blog.id AS blog_id FROM blog JOIN category ON blog.category_id = category.id JOIN user ON blog.user_id = user.id ORDER BY blog.create_time DESC')
+  // let [data] = await pool.execute('SELECT blog.*, category.category_name, user.*, blog.id AS blog_id FROM blog JOIN category ON blog.category_id = category.id JOIN user ON blog.user_id = user.id ORDER BY blog.create_time DESC')
 
-    
   res.json(data)
 })
 
 //連結 store,user,category,blog_comment這幾個資料表
 router.get('/:blogId', async (req, res) => {
   const blogId = req.params.blogId
-  let [data] = await pool.execute('SELECT * FROM blog WHERE id = ?',[blogId])
+  let [data] = await pool.execute('SELECT * FROM blog WHERE id = ?', [blogId])
   res.json(data)
 })
 
