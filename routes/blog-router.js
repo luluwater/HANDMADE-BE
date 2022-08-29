c81a904c9b5e89ab0c052287269511366197d87c
Angus <sickmi14798@gmail.com>
2022-08-29 18:17:32 +0800

    該動工囉!!

--- routes/blog-router.js
+++ routes/blog-router.js
@@ -1,31 +1,30 @@
 const router = require('express').Router()
-// const { index } = require('../controllers/blog-controller')
 const pool = require('../configs/mysql')
+// const { index } = require('../controllers/blog-controller')
 
-//RESful API
+//RESEful API
 
 // http://localhost:8080/api/blog --> 顯示所有部落格
 router.get('/', async (req, res) => {
-  // let [data] = await pool.execute('SELECT * FROM blog ORDER BY create_time DESC')
-  let [data] = await pool.execute('SELECT blog.*, user.*, blog.id AS blog_id FROM blog JOIN user ON blog.user_id = user.id ORDER BY blog.create_time DESC')
-
-  // let [data] = await pool.execute('SELECT blog.*, category.category_name, user.*, blog.id AS blog_id FROM blog JOIN category ON blog.category_id = category.id JOIN user ON blog.user_id = user.id ORDER BY blog.create_time DESC')
+  let [data] = await pool.execute(
+    'SELECT blog.*, category.category_name, user.*, blog.id AS blog_id FROM blog JOIN category ON blog.category_id = category.id JOIN user ON blog.user_id = user.id ORDER BY blog.create_time DESC'
+  )
 
   res.json(data)
 })
 
 //連結 store,user,category,blog_comment這幾個資料表
+//http://localhost:8080/api/blog/:blogId --> 顯示單一部落格
 router.get('/:blogId', async (req, res) => {
   const blogId = req.params.blogId
-  let [data] = await pool.execute('SELECT * FROM blog WHERE id = ?', [blogId])
+
+  let [data] = await pool.execute(
+    'SELECT blog.*, category.category_name, user.*, store.*, blog.id AS blog_id FROM blog JOIN category ON blog.category_id = category.id JOIN user ON blog.user_id = user.id JOIN store ON blog.store_id = store.id WHERE blog.id = ?',
+    [blogId]
+  )
   res.json(data)
 })
 
-// http://localhost:8080/api/blog --> 顯示所有部落格
-router.post('/:id', async (req, res, next) => {
-  let [data] = await pool.execute('SELECT * FROM blog')
-  res.json(data)
-  next()
-})
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const multer = require('multer')
 
 const chatRouter = require('./routes/chat-router')
 const blogRouter = require('./routes/blog-router')
+// const userRouter = require('./routes/user-router')
 
 const PORT = process.env.PORT || 8080
 
@@ -14,53 +14,9 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-/**
- * imgae
- */
-const pool = require('./configs/mysql')
-const router = require('express').Router()
-
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'public')
-  },
-  filename: function (req, file, cb) {
-    let ext = file.originalname.split('.')
-    ext = ext[ext.length - 1]
-    //用日期來給檔案名稱
-    cb(null, `${Date.now()}.${ext}`)
-  },
-})
-const upload = multer({ storage })
-var corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200,
-}
-app.use([express.static('public'), express.json(), cors(corsOptions), upload.array('files')])
-app.post('/upload_files', (req, res) => {
-  if (req.files.length > 0) {
-    res.json(req.files[0])
-    console.log(res.json(req.files[0]))
-  }
-})
-
-router.post('/upload_blog', async (req, res) => {
-  const content = req.body.content
-  const id = Date.now()
-
-  try {
-    let saveNameResult = pool.execute(`INSERT IGNORE INTO blog (id, content) VALUES (?, ?)`, [id, content])
-    console.log('成功輸入', saveNameResult)
-    console.log(id)
-  } catch (e) {
-    console.error(e)
-  }
-})
-
-////////////////////////////
-
 app.use('/api/chat', chatRouter)
 app.use('/api/blog', blogRouter)
+// app.use('/api/user', userRouter)
 
 app.get('/', (req, res) => {
   res.send('homepage')
