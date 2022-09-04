d58e582341170a23817286719f42caf045d97472
Angus <94235117+angushyx@users.noreply.github.com>
2022-09-04 21:39:41 +0800

    web socket

--- server.js
+++ server.js
@@ -2,16 +2,41 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
+const http = require('http')
+const { Server } = require('socket.io')
+
 const chatRouter = require('./routes/chat-router')
 const blogRouter = require('./routes/blog-router')
+
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
 
+
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    method: ['GET', 'POST'],
+  },
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+// socket 開始
+io.on('connection', (socket) => {
+
+  socket.emit('messageFormServer',{data:'Welcome to the socket io server'})
+  socket.on('messageToServer', (dataFormClient)=>{
+    
+  })
+})
+
+
 
 app.use('/api/chat', chatRouter)
 app.use('/api/blog', blogRouter)
@@ -19,6 +44,8 @@ app.use('/api/blog', blogRouter)
 app.get('/', (req, res) => {
   res.send('homepage')
 })
+
+server.listen(PORT, console.log(`webSocket has successfully Start at: ${PORT}`))
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
