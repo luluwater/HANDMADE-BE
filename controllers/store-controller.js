87f5e6cd3061eed62af2a4d43b3286274cd31847
RJ chen <cloudbay12@gmail.com>
2022-09-06 11:00:42 +0800

    crouseAPI update

--- controllers/store-controller.js
+++ controllers/store-controller.js
@@ -23,26 +23,24 @@ const getStore = async (req, res) => {
 
 const getStoreDetail = async (req, res) => {
   // console.log(req.params)
-  console.log(req)
+  // console.log('req', req)
   // console.log(req.query)
-  const id = req.params.id
-  console.log(id)
+  const id = req.params.storeId
+  // console.log(id)
 
   const [data] = await pool.execute(
-    'SELECT *, category.category_name, mrt.MRT_station, mrt.station_name FROM store JOIN category ON category.id = category_id JOIN mrt ON MRT_id = mrt.id WHERE id = ?',
+    'SELECT *, category.category_name, mrt.MRT_station, mrt.station_name FROM store JOIN category ON category.id = category_id JOIN mrt ON MRT_id = mrt.id WHERE store.id = ?',
     [id]
   )
 
-  const [kvImgs] = await pool.execute('SELECT * FROM store_kv')
-  const response = data.map((stores) => {
-    const newKvImg = kvImgs.filter((kv) => kv.store_id === stores.id)
-    const kv_name = newKvImg.map((kv) => kv.kv_img)
-
-    stores['kv_name'] = kv_name
-    return stores
+  const [kvImgs] = await pool.execute('SELECT * FROM store_kv WHERE store_id = ?', [id])
+  const courseDetail = data.map((v) => {
+    const kv_imgs = kvImgs.map((v2) => v2.kv_img)
+    v['kv_imgs'] = kv_imgs
+    return v
   })
 
-  res.json(response)
+  res.json(courseDetail)
 }
 
 module.exports = { getStore, getStoreDetail }
