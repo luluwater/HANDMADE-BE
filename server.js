436f9bc75f2af7699ae2c0e8fab7789af5879f7d
Angus <94235117+angushyx@users.noreply.github.com>
2022-09-07 22:33:39 +0800

    0907 for merge

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const http = require('http')
 const { Server } = require('socket.io')
@@ -28,12 +29,32 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // socket 開始
+/**
+ * namesSpace 很像 endpoint，但不是真正的 endpoint
+ * 可以透過不同的 namespace　來創建不同的　room
+ * join and leaving
+ */
 io.on('connection', (socket) => {
-
+  console.log('Some connected to the main namespace!!')
   socket.emit('messageFormServer',{data:'Welcome to the socket io server'})
   socket.on('messageToServer', (dataFormClient)=>{
-    
+    console.log(dataFormClient)
   })
+
+  socket.join('level1')
+  //當自己加進去 room 時也會顯示 msg
+  //第一層 of --> namespace
+  //第二層 to --> room
+  io.of('/').to('level1').emit('joined',`${socket.id} say I have joined the level 1 room!` )
+})
+
+/**
+ * 與上面唯一的不同就是 namespace
+ * room 是只有 server 才有的
+ */
+io.of('/admin').on('connection', socket=>{
+  console.log('Some connected to the admin namespace!!')
+  io.of('/admin').emit('welcome','welcome to the admin channel!')
 })
 
 
@@ -45,8 +66,8 @@ app.get('/', (req, res) => {
   res.send('homepage')
 })
 
-server.listen(PORT, console.log(`webSocket has successfully Start at: ${PORT}`))
+server.listen(8000, console.log(`webSocket has successfully Start at: ${8000}`))
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
+  console.log(`node Server is running on http://localhost:${PORT}`)
 })
