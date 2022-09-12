57503164c56150934674b371aa2fddac1e96b53f
黃昱銘 <dive880217@gmail.com>
2022-09-12 17:01:54 +0800

    login be

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+require('dotenv').config()
 
 const chatRouter = require('./routes/chat-router')
 const blogRouter = require('./routes/blog-router')
+//Login
+const Login = require('./routes/Login')
 
 const PORT = process.env.PORT || 8080
+
+const expressSession = require('express-session');
+// 把 session 存在硬碟中
+var FileStore = require('session-file-store')(expressSession);
+app.use(
+  expressSession({
+    store: new FileStore({
+      // session 儲存的路徑
+      path: path.join(__dirname, '..', 'sessions'),
+    }),
+    secret: process.env.SESSION_SECRET,
+    // 如果 session 沒有改變的話，要不要重新儲存一次？
+    resave: false,
+    // 還沒初始化的，要不要存
+    saveUninitialized: false,
+  })
+);
+
+
 
 const app = express()
 
@@ -15,6 +37,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api/chat', chatRouter)
 app.use('/api/blog', blogRouter)
+//Login
+app.use('/api/Login', Login)
 
 app.get('/', (req, res) => {
   res.send('homepage')
