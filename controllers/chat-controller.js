86777f23c63318ecbef1c91448e659ea5c7774bf
Angus <sickmi14798@gmail.com>
2022-09-14 20:59:09 +0800

    feat:chat insert

--- controllers/chat-controller.js
+++ controllers/chat-controller.js
@@ -1,10 +1,29 @@
 const pool = require('../configs/mysql')
 
+// TODO: isPrivate 加入
 const getChatRoom = async (req, res) => {
-  let [data] = await pool.execute('SELECT rooms.*, message.*, rooms.id AS rooms_id FROM rooms JOIN message ON rooms.rooms_id = message.room_id')
-  res.json(data)
+  let [rooms] = await pool.execute('SELECT rooms.* FROM rooms')
+  let [msg] = await pool.execute('SELECT message.*,user.* FROM message JOIN user ON message.user_id = user.id')
+
+  for (let i = 0; i < rooms.length; i++) {
+    rooms[i].msg = msg.filter((m) => m.room_id === rooms[i].id)
+  }
+
+  res.json(rooms)
+}
+
+const sendChatMessage = async (req, res) => {
+  const { id, content, user_id, created_at, room_id } = req.body
+
+  console.log(req.body)
+
+  await pool.execute(`INSERT IGNORE INTO message (id, content, user_id, created_at, room_id ,isPrivate) VALUES (?, ?, ?, ? , ?, 0 )`, [id, content, user_id, created_at, room_id])
+
+  console.log('INSERT chatMsg success')
+  res.send('INSERT chatMsg success')
 }
 
 module.exports = {
   getChatRoom,
+  sendChatMessage,
 }
--- routes/chat-router.js
+++ routes/chat-router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
-const { getChatRoom } = require('../controllers/chat-controller')
+const { getChatRoom, sendChatMessage } = require('../controllers/chat-controller')
 
 router.get('/', getChatRoom)
+
+router.post('/msg', sendChatMessage)
 
 module.exports = router
