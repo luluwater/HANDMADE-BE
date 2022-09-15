c22439529dfa0eb5b8f8325d70e05402a54054e1
lulu <water0452@gmail.com>
2022-09-15 16:55:13 +0800

    userapi

--- controllers/user-likes-controller.js
+++ controllers/user-likes-controller.js
@@ -29,6 +29,7 @@ const userLikesProduct = async (req, res) => {
     'SELECT user_favorite_product.*, category.category_en_name, product.store_id, store.name AS store_name, product.name AS product_name, product.price FROM user_favorite_product JOIN user ON user_favorite_product.user_id = user.id JOIN category ON user_favorite_product.category_id = category.id JOIN product ON user_favorite_product.product_id = product.id JOIN store ON user_favorite_product.store_id = store.id WHERE user.id = 1'
   )
   const productImgs = await pool.execute(`SELECT * FROM product_img`)
+  //TODO:參數改session
   const dataProductDetail = data[0].map((v) => {
     const newImgs = productImgs[0].filter((v2) => v2.product_id === v.product_id)
     const newImagsName = newImgs.map((v2) => v2.img_name)
