c08a90018579e4ea5159c9ad90455dce6626d608
Angus <sickmi14798@gmail.com>
2022-09-26 20:59:03 +0800

    asfa

--- configs/socket.js
+++ configs/socket.js
@@ -1,5 +1,4 @@
 const { Server } = require('socket.io')
-const { getChatRoomTest } = require('../controllers/chat-controller')
 
 const SocketServer = async (server) => {
   const io = new Server(server, {
@@ -10,26 +9,34 @@ const SocketServer = async (server) => {
     },
   })
 
-  let rooms = await getChatRoomTest()
-
   io.on('connection', (socket) => {
     socket.emit('roomMsg', '歡迎加入')
 
     socket.on('roomMsg', (roomMsg) => {
       socket.join(roomMsg.roomName)
 
+      socket.broadcast.to(roomMsg.roomName).emit('joinData', roomMsg)
       socket.broadcast.to(roomMsg.roomName).emit('roomMsg', `${roomMsg.user?.account} 已加入 ${roomMsg.roomName}`)
-    })
-
-    socket.on('sendMsg', (sendMsg) => {
-      // io.emit('sendMsg', sendMsg?.room_id)
-      console.log('sendMsg.room_id', sendMsg.room_id)
-      io.to(sendMsg.room_id).emit('sendMsg', sendMsg)
     })
 
     socket.on('left', (data) => {
       io.emit('leftMsg', `${data.user.account} 已離開 ${data.room.room_title}`)
     })
+
+    socket.on('leftRoom', (data) => {
+      io.emit('leftData', data)
+    })
+
+    socket.on('sendMsg', (sendMsg) => {
+      socket.join(sendMsg.room_title)
+      io.to(sendMsg.room_title).emit('chat', sendMsg)
+    })
+
+    // socket.on('left', (data) => {
+    //   socket.join(data.room.room_title)
+    //   io.emit('leftMsg', data)
+    //   // io.emit('leftData', data)
+    // })
   })
 }
 
